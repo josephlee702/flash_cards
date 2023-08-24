be4042d5c5fb7607537961e80f2a1a65ba8c9af7
Joseph Lee <jhjlee702@gmail.com>
2023-08-23 19:45:23 -0600

    Finalize code

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,16 +1,17 @@
 class Deck
-  attr_reader :cards
+  attr_reader :cards 
 
   def initialize(cards)
     @cards = cards
   end
 
   def count
-    cards.count
+    @cards.count
   end
 
-  def cards_in_category(x)
-    sefdfdsfds
+  def cards_in_category(category)
+    @cards.select do |card|
+      card.category == category
     end
   end
 end
