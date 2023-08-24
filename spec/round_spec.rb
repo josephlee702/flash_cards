cafc76ccd0da136c9489348a557306ca4032db0c
Joseph Lee <jhjlee702@gmail.com>
2023-08-23 22:52:36 -0600

    is it correct

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -22,4 +22,26 @@ RSpec.describe Round do
     expect(round.turns).to eq([])
     expect(round.current_card).to eq(card_1)
   end
+
+  it 'create new turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    expect(round.turns).to eq([])
+    expect(round.current_card).to eq(card_1)
+    new_turn = round.take_turn("Juneau")
+    expect(new_turn).to be_instance_of(Turn)
+  end
+
+  it 'is it correct?' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    expect(new_turn.correct?).to be true
+  end
 end
