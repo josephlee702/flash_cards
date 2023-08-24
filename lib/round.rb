d07b9ac61f6c46147e8ff60980ff004b6bc0b37b
Joseph Lee <jhjlee702@gmail.com>
2023-08-23 21:11:22 -0600

    current card method and attribute defintions

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,20 @@
+require '../lib/turn'
+
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
   
   def initialize(deck)
     @deck = deck
+    @turns = []
+    @current_turn = 0
   end
 
-  # def current_card()
+  def current_card
+    @deck.cards[@current_turn]
+  end
 end
+
+# current_card - function
+# take_turn - function
+# turns - array - attribute
+# number_correct - attribute
