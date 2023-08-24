cdeed5a15e236778224a0824916566da4d5426e8
Joseph Lee <jhjlee702@gmail.com>
2023-08-23 19:57:46 -0600

    Finish processes responses and records guesses

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,13 +4,12 @@ require '../lib/deck'
 require '../lib/round'
 
 RSpec.describe Round do
-  it 'process responses and records guesses' do
+  it 'processes responses and records guesses' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
+    deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
-    expect(deck.cards).to eq(cards)
+    expect(round.deck).to eq(deck)
   end
 end
